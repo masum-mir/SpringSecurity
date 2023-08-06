3449bb1c3c9057b4e2c7fa3eb62bfd4b2f823795
masum <masummir.lm@gmail.com>
2023-08-06 11:13:40 +0600

    update code

--- src/main/java/com/security/SpringRestApplication.java
+++ src/main/java/com/security/SpringRestApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
-import java.util.Collections;
+import java.util.*;
 
 @SpringBootApplication
 @ComponentScan(basePackages = {"com.security.*"})
@@ -18,6 +18,22 @@ public class SpringRestApplication {
         SpringApplication app = new SpringApplication(SpringRestApplication.class);
         app.setDefaultProperties(Collections.singletonMap("server.port", "9091"));
         app.run(args);
+
+        String[] nm = {"nm1","nm2","nm3","nm4","nm5",};
+
+        Map<String, String> maping = new HashMap<>();
+
+        for(String n: nm) {
+            String repo = n + "_Repo";
+            maping.put(n, repo);
+        }
+
+        for(String n: nm ) {
+            String repoName = maping.get(n);
+                System.out.println(repoName);
+
+        }
+
     }
 
 }
